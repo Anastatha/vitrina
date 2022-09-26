58bd8a19e2ff39d0d4e09ee4e27a770484b6c6b3
Anastatha <100115542+Anastatha@users.noreply.github.com>
2022-09-26 17:48:39 +0300

    Add files via upload

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react';
+import {ShopContext} from '../context';
+
 function GoodsItem(props) {
     const {
         id,
         name,
         description,
         price,
-        full_background,
-        addToBasket = Function.prototype,
+        full_background
     } = props;
+
+    const {addToBasket} = useContext(ShopContext);
 
     return (
         <div className='card'>
